12d5efa5e2e73d941d999c82cf19349881b9d1c6
1huanle <1huanle@users.noreply.github.com>
2026-08-24 10:38:48 +0800

    test文件夹只保留main_write.py

--- tests/test_save_login_state.py
+++
@@ -1,7 +0,0 @@
-from save_login_state import is_management_page_url
-
-
-def test_management_page_url_allows_query_parameters():
-    assert is_management_page_url(
-        "https://yunjingzhi.com/yunjingservice/user/txform.shtml?source=login"
-    )
