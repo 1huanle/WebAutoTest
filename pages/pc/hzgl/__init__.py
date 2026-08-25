134b1786eed113b931a962ff1ae6b3dbd7e59d26
1huanle <1huanle@users.noreply.github.com>
2026-08-25 17:52:15 +0800

    pc端新增“新建诊断”自动化

--- pages/pc/hzgl/__init__.py
+++ pages/pc/hzgl/__init__.py
@@ -1,6 +1,8 @@
 """PC 端患者管理页面对象。"""
 
 from .hzgl_page import HzglPage
+from .hzgl_jbzd_pages import DiagnosisFormSection, DiagnosisListSection, HzglJbzdPage
+from .hzgl_yz import HzglYzPage, YzFormSection, YzListSection
 from .patient_detail_dialog import PatientDetailDialog
 from .patient_filter_section import PatientFilterSection
 from .patient_list_section import PatientCard, PatientListSection
@@ -8,6 +10,12 @@ from .patient_create_page import PatientCreatePage
 
 __all__ = [
     "HzglPage",
+    "HzglJbzdPage",
+    "DiagnosisListSection",
+    "DiagnosisFormSection",
+    "HzglYzPage",
+    "YzListSection",
+    "YzFormSection",
     "PatientCard",
     "PatientDetailDialog",
     "PatientCreatePage",
